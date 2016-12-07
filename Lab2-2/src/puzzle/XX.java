9f9a8dc247c0cffc7d898df897dea37952084581
dumbnut7410 <gilderjw@rose-hulman.edu>
2016-12-07 12:50:49 -0500

    added lab 2-2

---
+++ Lab2-2/src/puzzle/XX.java
@@ -0,0 +1,10 @@
+package puzzle;
+public class XX {
+    public static void main(String[] args) {
+        char x = 'X';
+        int i = 0;
+        // prints XX
+        System.out.print(true  ? x : 0);
+        System.out.print(false ? i : x); 
+    }
+}
