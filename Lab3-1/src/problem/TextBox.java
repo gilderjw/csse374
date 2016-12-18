31198ea58edba0c57afb3c77fb387cedd5de7319
dumbnut7410 <gilderjw@rose-hulman.edu>
2016-12-18 16:05:32 -0500

    started lab3-1

---
+++ Lab3-1/src/problem/TextBox.java
@@ -0,0 +1,110 @@
+package problem;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+
+/**
+ * This class provides the support for the text box widget.
+ *  
+ * @author Chandan R. Rupakheti (chandan.rupakheti@rose-hulman.edu)
+ */
+public class TextBox extends AbstractComponent {
+
+	private static final int H_SPACE = 5;
+
+	private String text;
+	
+	public TextBox() {
+		this(null);
+	}
+
+	public TextBox(String text) {
+		this(text, null);
+	}
+
+	public TextBox(String text, Rectangle bound) {
+		this(null, text, bound);
+	}
+
+	public TextBox(IComponent parent, String text, Rectangle bound) {
+		super(parent, bound);
+		
+		this.text = text;
+		
+		if(this.text == null)
+			this.text = "";
+	}
+	
+	/**
+	 * Gets the text in the label.
+	 */
+	public String getText() {
+		return this.text;
+	}
+	
+	/**
+	 * Sets the text in the label. 
+	 * Calling this method results in the call to {@link #fireUpdate()}, 
+	 * which informs the component hierarchy to re-draw itself.
+	 */
+	public void setText(String text) {
+		this.text = text;
+		this.fireUpdate();
+	}
+
+	@Override
+	public void drawComponent(Graphics2D g) {
+		if(Configuration.getOS().contains("windows"))
+			this.drawForMSWindow(g);
+		else if(Configuration.getOS().contains("ubuntu"))
+			this.drawForUbuntu(g);
+		else
+			throw new UnsupportedOperationException("The GUI framework  does not yet support your operating system!");
+	}
+
+	private void drawForMSWindow(Graphics2D g) {
+		Rectangle bound = this.getBounds();
+		
+		
+		// Draw the boarder after setting the thickness
+		g.setColor(new Color(14,29,110));
+		Rectangle2D border = new Rectangle2D.Float(bound.x, bound.y, bound.width, bound.height);
+		g.setStroke(new BasicStroke(5));
+		g.draw(border);
+		
+		// Draw the white fill
+		g.setColor(Color.white);
+		Rectangle2D fill = new Rectangle2D.Float(bound.x+2, bound.y+2, bound.width-2, bound.height-2);
+		g.fill(fill);;
+		
+		// Draw the Fill
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 12)); 		
+		g.setColor(Color.black);
+		g.drawString(this.text, bound.x + H_SPACE, bound.y + 16);
+	}
+
+	private void drawForUbuntu(Graphics2D g) {
+		Rectangle bound = this.getBounds();
+		
+		
+		// Draw the boarder after setting the thickness
+		g.setColor(new Color(48,0,0));
+		Rectangle2D border = new Rectangle2D.Float(bound.x, bound.y, bound.width, bound.height);
+		g.setStroke(new BasicStroke(5));
+		g.draw(border);
+		
+		// Draw the white fill
+		g.setColor(Color.white);
+		Rectangle2D fill = new Rectangle2D.Float(bound.x+2, bound.y+2, bound.width-2, bound.height-2);
+		g.fill(fill);;
+		
+		// Draw the Fill
+		g.setFont(new Font("Arial", Font.PLAIN, 12)); 		
+		g.setColor(Color.black);
+		g.drawString(this.text, bound.x + H_SPACE, bound.y + 16);
+	}
+}
---
+++ Lab3-1/src/problem/Window.java
@@ -0,0 +1,106 @@
+package problem;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * This class provides the support for the main window widget and also serves as
+ * the top-level component in the component hierarchy.
+ *  
+ * @author Chandan R. Rupakheti (chandan.rupakheti@rose-hulman.edu)
+ */
+public class Window extends AbstractComponent {
+	public static final int V_SPACE = 3;
+	public static final int H_SPACE = 3;
+	public static final int TITLE_HEIGHT = 25;
+	
+	private String title;
+	private JFrame frame;
+	private JPanel panel;
+
+	public Window() {
+		this(null);
+	}
+
+	public Window(String title) {
+		this(title, null);
+	}
+
+	@SuppressWarnings("serial")
+	public Window(String title, Rectangle bound) {
+		super(null, bound);
+
+		if(title == null)
+			this.title = "";
+		else
+			this.title = title;
+
+		this.frame = new JFrame(this.title);
+		this.frame.setUndecorated(true);	
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.panel = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				draw((Graphics2D) g);
+			}
+		};
+		this.frame.setContentPane(this.panel);
+	}
+
+	@Override
+	public final void fireUpdate() {
+		this.panel.repaint();
+	}
+	
+	public void show() {
+		this.frame.setBounds(this.getBounds());
+		this.frame.setVisible(true);
+	}
+	
+	@Override
+	public void drawComponent(Graphics2D g) {
+		if(Configuration.getOS().contains("windows"))
+			this.drawForMSWindow(g);
+		else if(Configuration.getOS().contains("ubuntu"))
+			this.drawForUbuntu(g);
+		else
+			throw new UnsupportedOperationException("The GUI framework  does not yet support your operating system!");
+	}
+
+	private void drawForMSWindow(Graphics2D g) {
+		// Draw the border
+		g.setColor(Color.gray);
+		g.draw3DRect(1, 1, this.getBounds().width - H_SPACE, (int)this.getBounds().height - V_SPACE, true);
+
+		// Draw the title bar
+		g.setColor(new Color(49, 68, 176));
+		g.fill3DRect(1, 1, this.getBounds().width - H_SPACE , TITLE_HEIGHT - 5, true);
+		
+		// Draw the title
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 15)); 		
+		g.setColor(Color.white);
+		g.drawString(this.title, 5, 17);
+	}
+
+	private void drawForUbuntu(Graphics2D g) {
+		// Draw the border
+		g.setColor(new Color(48,0,0));
+		g.draw3DRect(1, 1, this.getBounds().width - H_SPACE, this.getBounds().height - V_SPACE, true);
+
+		// Draw the title bar
+		g.setColor(new Color(96,0,0));
+		g.fill3DRect(1, 1, (int)this.getBounds().width - H_SPACE , TITLE_HEIGHT, true);
+		
+		// Draw the title
+		g.setFont(new Font("Arial", Font.PLAIN, 17)); 		
+		g.setColor(Color.white);
+		g.drawString(this.title, 5, 20);
+	}
+}
