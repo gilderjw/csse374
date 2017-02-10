f13c7aa94d8adfe485b8638bc8578b0b3853f586
dumbnut7410 <gilderjw@rose-hulman.edu>
2017-02-09 20:21:54 -0500

    done with exam

--- Exam3/src/problem/Command.java
+++ Exam3/src/problem/Command.java
@@ -1,8 +1,22 @@
 package problem;
 
-public interface Command {
+import java.util.Iterator;
+
+public interface Command extends Iterable<Command> {
 	// Preserve these method signatures.
 	public void execute();
 	public void undo();
 	// Add methods with default implementations below as needed.
+	
+	default void addChildCommand(Command c) {
+		throw new UnsupportedOperationException();
+	}
+	
+	default void removeChildCommand(Command c) {
+		throw new UnsupportedOperationException();
+	}
+	
+	default Iterator<Command> iterator(){
+		return new NullIterator();
+	}
 }
